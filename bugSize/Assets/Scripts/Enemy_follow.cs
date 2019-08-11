edda8e96957ad31d0ea2912ee434f0cb63913698
RubyArt <ruby_art25@yahoo.com.mx>
2019-08-10 22:41:26 -0500

    BugSize_v1

---
+++ bugSize/Assets/Scripts/Enemy_follow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_follow : MonoBehaviour
+{
+    public float speed;
+
+    private Transform target;
+    /*__________Start___________*/
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+    }/*__________Start___________*/
+    /*__________Update___________*/
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+    }
+}
