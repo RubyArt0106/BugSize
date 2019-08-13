e0291bf5a5f39f7f27f63d808e927cb2cfc6ecf4
Alexis <alex.tover@gmail.com>
2019-08-13 03:09:54 -0500

    BugSize_v1.1.3
    Enemy detecta colision y carga la animacion de ataque

---
+++ bugSize/Assets/Scripts/enemy_Attack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_Attack : MonoBehaviour
+{
+
+    public GameObject aCheck;
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals("Player"))
+        {
+            Debug.Log("Attack_true");
+            anim.SetBool("isAttack", true);
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals("Player"))
+        {
+            Debug.Log("Attack_false");
+            anim.SetBool("isAttack", false);
+        }
+    }
+}
